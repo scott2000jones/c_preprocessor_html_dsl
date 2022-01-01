18a8f15051430d311fdb3872ec6c247d197fdd41
Scott Jones <scott2000jones@gmail.com>
2022-01-01 22:32:10 +0000

    Initial 'Tag' macro using preprocessor string concatenation

--- preprocessor_html.c
+++ preprocessor_html.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 
-
-#define Tag(name, block) printf("<%s>\n", name); block printf("</%s>\n", name);
-
-/* Taken from https://stackoverflow.com/a/32665978 */
-#define SCHEMA "test"
-#define TABLE  "data"
-
-const char *table = SCHEMA "." TABLE ; // note no + or . or anything
-const char *qry =               // include comments in a string
-    " SELECT *"                // get all fields
-    " FROM " SCHEMA "." TABLE   /* the table */
-    " WHERE x = 1 "             /* the filter */ 
-                ;
-/* End */
+#define Tag(name, child_tags) "<"name">\n"child_tags"</"name">\n"
 
 int main() {
     printf("Hello from the C preprocessor world\n");
-    Tag("html", {
-        Tag("header", {
+    printf("%s\n", Tag("html",
+        Tag("header",
             Tag("title",)
-        })
+        )
         Tag("body",)
-    });
-    printf("%s\n", qry);
+    ));
     return 0;
 }
