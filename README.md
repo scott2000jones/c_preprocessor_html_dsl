ccdf5938092dacf1de625ac4638059102133dd59
Scott Jones <scott2000jones@gmail.com>
2022-01-02 15:45:17 +0000

    Moved macros into header, moved example code into demo.c

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # C Preprocessor HTML DSL
 A simple domain-specific language (DSL) for writing HTML in C/C++, implemented using the C preprocessor (macros).
+
+Example:
+```
+printf(
+    DOCTYPE("html")
+    TAG("html",
+        TAG("header",
+            BASE("www.google.com", "_blank")
+            META(, "refresh", "30", )
+        )
+        TAG("body", 
+            TAG("div", 
+                A_HREF("www.google.com", 
+                    TAG("p", "lorem ipsum lorem ipsum")
+                )
+            )
+            IMG("icon.png", "a really cool icon")
+            UNARY_TAG("br")
+        )
+    )
+);
+```
---
+++ demo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "preprocessor_html.h"
+
+int main(void) {
+    char *generated_tags = calloc(1, 256);
+    for (int i = 0; i < 2; i++) {
+        strcat(generated_tags, TAG("li", "generated_item\n"));
+    }
+
+    printf(
+        DOCTYPE("html")
+        TAG("html",
+            TAG("header",
+                BASE("www.google.com", "_blank")
+                META(, "refresh", "30", )
+            )
+            TAG("body", 
+                TAG("ul", "%s")
+                TAG("div", 
+                    A_HREF("www.google.com", 
+                        TAG("p", "lorem ipsum lorem ipsum")
+                    )
+                )
+                IMG("icon.png", "a really cool icon")
+                UNARY_TAG("br")
+            )
+        ), 
+        generated_tags);
+
+    free(generated_tags);
+
+    return 0;
+}
